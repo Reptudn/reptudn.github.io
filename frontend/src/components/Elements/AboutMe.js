add55917ce4662418accf035b02db4bf086b8ee3
Jonas <jonas.kauker2003@gmail.com>
2024-02-12 18:36:30 +0100

    added disco animation

--- frontend/src/components/Elements/AboutMe.js
+++ frontend/src/components/Elements/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MainImage from '../../assets/images/jonas.JPG';
+import MainImage from '../../assets/images/tudn.jpg';
 import './AboutMe.css';
 
 class AboutMe extends React.Component {
@@ -10,7 +10,7 @@ class AboutMe extends React.Component {
         <img src={MainImage} alt="Jonas Kauker" />
         <div className="content">
           <h1>Jonas Kauker</h1>
-          <h2>Full Stack Developer</h2> {/* Replace this line with "I love <whatever i love>  This is supposed to change and has multiple options and it will animted in as if its being typed" */}
+          <h2>Disco Time!</h2> {/* Replace this line with "I love <whatever i love>  This is supposed to change and has multiple options and it will animted in as if its being typed" */}
         </div>
       </div>
     );

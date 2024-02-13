0706bd468249276330e3bbca89f2c4b34054ea0f
Jonas <jonas.kauker2003@gmail.com>
2024-02-13 08:25:54 +0100

    added three.js scene with update function

--- frontend/src/components/Scenes/Scene.js
+++ frontend/src/components/Scenes/Scene.js
@@ -1,46 +1,46 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, {  useEffect, useRef } from "react";
 import * as THREE from "three";
 
 function Scene() {
     
+    const canvasRef = useRef(null);  
     const animationFrameId = useRef(null);
-    
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        canvas: document.querySelector('#scene')
-    });
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setPixelRatio( window.devicePixelRatio );
-
-    var ballGeometry = new THREE.SphereGeometry(2.5, 32, 32);
-    var ballMaterial = new THREE.MeshStandardMaterial({color: 0x00ff00, wireframe: true});
-    var ball = new THREE.Mesh(ballGeometry, ballMaterial);
-    scene.add(ball);
-
-    var light = new THREE.AmbientLight(0xffffff, 1, 100);
-    light.position.set(0, 0, 0);
-    scene.add(light);
-
-    camera.position.z = 5;
-    renderer.render(scene, camera);
-    
-    const update = useCallback(() => {
-        ball.rotation.x += 0.01;
-        ball.rotation.y += 0.01;
-        renderer.render(scene, camera);
-        animationFrameId.current = window.requestAnimationFrame(update);
-    }, []);   
 
     useEffect(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+        const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const Cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+        scene.add(Cube);
+
+        camera.position.z = 5;
+
+        // on update function
+        const update = () => {
+
+            Cube.rotation.x += 0.01;
+            Cube.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+            animationFrameId.current = window.requestAnimationFrame(update);
+        };
+
+        update();
+
         animationFrameId.current = window.requestAnimationFrame(update);
         return () => window.cancelAnimationFrame(animationFrameId.current);
-    }, [update]);
+    }, []);
 
     return (
-        <canvas id="scene" onClick={renderer.render(scene, camera)}/>
+        <canvas id="scene" ref={canvasRef}/>
     );
 }
 
